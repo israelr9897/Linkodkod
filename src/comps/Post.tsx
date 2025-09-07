f299763a0212a2748e663c4c412f0c1577562d2b
israelr9897 <israelr9897@gmail.com>
2025-09-07 12:57:46 +0300

    added responsiv

--- src/comps/Post.tsx
+++ src/comps/Post.tsx
@@ -1,5 +1,19 @@
 import "../assets/styles/post.css";
 import { useState } from "react";
+import worldImg from "../assets/images/world.png"
+
+export type Post = {
+  discrption: string;
+  name: string;
+  date: string;
+};
+
+const post: Post = {
+  // imgUrl:,
+  discrption: "Hello World",
+  name: "Israel israeli",
+  date: "07/09/2025",
+};
 
 export default function Post() {
   const [count, setCount] = useState(0);
@@ -7,14 +21,14 @@ export default function Post() {
     <div className="main-post">
       <div className="post">
         <div className="head-post">
-          <img className="logo-img" src="" alt="logo-img" />
+          <img className="logo-img" src={worldImg} alt="logo-img" />
           <div className="description">
-            <h1>description</h1>
+            <h1>{post.discrption}</h1>
           </div>
         </div>
         <div className="details">
-          <p className="name">Name:</p>
-          <p className="date">Created in:</p>
+          <p className="name">Name: {post.name}</p>
+          <p className="date">Created in: {post.date}</p>
         </div>
         <div className="likes">
           <div className="like" onClick={() => setCount((prev) => prev + 1)}>
